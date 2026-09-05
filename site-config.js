561fd1637eae388595c80ecc0f733c4faf0d565f
Bonzifyer <133811496+10minecraft29@users.noreply.github.com>
2026-09-05 13:25:04 -0400

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,7 +1,7 @@
 window.BONZI_SITE = {
   version: '4.1.4',
-  downloadUrl: 'https://github.com/10minecraft29/bonzibrowsersite/releases/download/1/BonziBrowser-Setup-4.1.4.exe',
-  releaseUrl: 'https://github.com/10minecraft29/bonzibrowsersite/releases/tag/1',
+  downloadUrl: 'https://github.com/10minecraft29/bonzibrowsersite/releases/download/2/BonziBrowser-Setup-4.1.4.exe',
+  releaseUrl: 'https://github.com/10minecraft29/bonzibrowsersite/releases/tag/2',
   releasesUrl: 'https://github.com/10minecraft29/bonzibrowsersite/releases',
   installerName: 'BonziBrowser-Setup-4.1.4.exe',
   installerSize: 'Approx. 110 MB',
